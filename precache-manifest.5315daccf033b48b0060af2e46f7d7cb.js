749d26e9650b23ce7fe55f43e2439518fe21212b
KaroGevorgyan91 <53303311+KaroGevorgyan91@users.noreply.github.com>
2019-11-22 11:28:26 +0400

    Updates

---
+++ precache-manifest.5315daccf033b48b0060af2e46f7d7cb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bfd189a51c9bbf43da866bb4168f74a",
+    "url": "/KaroGevorgyan91/Todo-react/index.html"
+  },
+  {
+    "revision": "f97d1e63b9b7cc086c48",
+    "url": "/KaroGevorgyan91/Todo-react/static/js/2.ff2a79be.chunk.js"
+  },
+  {
+    "revision": "d9c1815af1ae88cbcf00",
+    "url": "/KaroGevorgyan91/Todo-react/static/js/main.4c2aa53b.chunk.js"
+  },
+  {
+    "revision": "63170375491b898d3394",
+    "url": "/KaroGevorgyan91/Todo-react/static/js/runtime-main.69b8e4dc.js"
+  }
+]);
